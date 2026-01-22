c664358e583e85c1a3941fd9f4836c3315259b3b
허준형 <gjwnsgud38@gmail.com>
2026-01-22 15:31:07 +0900

    Feat: 기본 구조, 의존성 추가

---
+++ snwa-backend/src/test/java/com/team/snwa/snwabackend/SnwaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.snwa.snwabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnwaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
