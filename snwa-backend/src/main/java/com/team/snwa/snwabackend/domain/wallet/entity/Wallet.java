a3c7d1586eadb80d1aaa8986e86233c5a60ddaea
simmina <s1mm1n6h@gmail.com>
2026-01-27 11:05:06 +0900

    feat: coin 도메인 완성, 스켈레톤 코드 작성

--- snwa-backend/src/main/java/com/team/snwa/snwabackend/domain/wallet/entity/Wallet.java
+++ snwa-backend/src/main/java/com/team/snwa/snwabackend/domain/wallet/entity/Wallet.java
@@ -1,20 +1,51 @@
 package com.team.snwa.snwabackend.domain.wallet.entity;
 
 import com.team.snwa.snwabackend.domain.user.entity.User;
+import com.team.snwa.snwabackend.global.common.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 
 @Entity
-@Table(name = "wallets")
+@Table(
+        name = "wallets",
+        uniqueConstraints = @UniqueConstraint(columnNames = "user_id")
+)
 @Getter
-public class Wallet {
+public class Wallet extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    @Column(nullable=false)
     private Long balance;
+
+    /**
+     * 낙관적 락을 위한 버전 필드
+     * - 동일 엔티티에 대한 동시 수정 시 충돌을 감지하기 위해 사용
+     * - 업데이트 시 version 값을 비교하여 중간 변경이 발생한 경우 예외를 발생시킴
+     * - 지갑 잔액, 코인 수량 등 동시성 문제가 발생할 수 있는 도메인에서 사용
+     */
+    @Version
+    private Long version;
+
+    protected  Wallet() {
+    }
+
+    /**
+     * 사용자의 지갑을 최초로 생성함
+     * 사용자당 하나의 지갑만 생성됨
+     * 초기 잔액은 0으로 설정
+     */
+    public static Wallet create(User user) {
+        Wallet wallet = new Wallet();
+        wallet.user = user;
+        wallet.balance = 0L;
+        return wallet;
+    }
+
+
 }
